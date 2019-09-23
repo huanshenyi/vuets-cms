141016ef406633b2ea427a3438ec3e874c7d874b
huanshenyi <txy1226052@gmail.com>
2019-09-23 19:24:11 +0900

    create project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        home
+    </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+<script>
+import {Component, Vue } from "vue-property-decorator"
 @Component({
-  components: {
-    HelloWorld,
-  },
+    component:{}
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped>
+
+</style>
