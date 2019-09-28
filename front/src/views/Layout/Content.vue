16166fef236c25eed3ea52be4db71acba73f9fde
huanshenyi <txy1226052@gmail.com>
2019-09-28 10:16:20 +0900

    sidebar and contend layout ok

--- front/src/views/Layout/Content.vue
+++ front/src/views/Layout/Content.vue
@@ -1,17 +1,67 @@
 <template>
+    <el-container class="layout-content">
+        <!--左側のメニュー-->
+        <el-aside width="200px">
+            <slot name="left">
 
+            </slot>
+        </el-aside>
+        <!--右側のページ-->
+        <el-main>
+            <!--パンくずリスト-->
+            <div class="top">
+                <i class="el-icon-s-unfold"></i>
+                <el-breadcrumb class="breadcrumb" separator="/">
+                    <el-breadcrumb-item>イベント管理</el-breadcrumb-item>
+                    <el-breadcrumb-item>イベント管理1</el-breadcrumb-item>
+                    <el-breadcrumb-item>イベント管理2</el-breadcrumb-item>
+                </el-breadcrumb>
+            </div>
+            <!--内容-->
+            <div class="content">
+                <slot name="content"></slot>
+            </div>
+        </el-main>
+    </el-container>
 </template>
 
-<script>
+<script lang="ts">
 import { Component,Vue } from "vue-property-decorator"
 @Component({
-    component:{}
+    components:{}
 })
 export default class Content extends Vue{
 
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .layout-content {
+        width: 100%;
+        height: 100%;
+    .el-main {
+        padding: 0;
+    .top {
+        background: #fff;
+        height: 54px;
+        border-right: none;
+        border-bottom: 1px solid #e6e6e6;
+        display: flex;
+        align-items: center;
+    i {
+        font-size: 20px;
+        cursor: pointer;
+        padding-left: 16px;
+    }
+    .breadcrumb {
+        padding-left: 16px;
+    }
+    }
+    .content {
+        padding: 10px;
+        height: calc(100% - 54px);
+        box-sizing: border-box;
+    }
+    }
+    }
 </style>
--- front/src/views/Layout/index.vue
+++ front/src/views/Layout/index.vue
@@ -3,14 +3,25 @@
         <!--header-->
         <layout-header></layout-header>
         <!--内容-->
+        <Content>
+          <Sidebar slot="left"></Sidebar>
+         <router-view slot="content"></router-view>
+        </Content>
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import {Component,Vue } from 'vue-property-decorator'
 import layoutHeader from "@/views/Layout/LayoutHeader.vue";
+import Content from "@/views/Layout/Content.vue";
+import Sidebar from "@/views/Layout/Sidebar.vue";
+
 @Component({
-    components: {layoutHeader},
+    components: {
+        layoutHeader,
+        Content,
+        Sidebar
+    },
 })
 export default class Layout extends Vue{}
 </script>
